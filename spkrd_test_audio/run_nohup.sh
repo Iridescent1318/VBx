1e28914a68d686e007a25ee1d5fad7726bd588df
Iridescent <lzty19981106@qq.com>
2021-04-20 18:37:24 +0800

    nohup shell script update

--- spkrd_test_audio/run_nohup.sh
+++ spkrd_test_audio/run_nohup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-dataset=callhome2000
-ffactor=0.05
+dataset=amicorpus
+ffactor=0.1
+fvariable=xvector
 
-nohup ./run_formal.sh ${dataset} ${ffactor} > run_formal_${dataset}_${ffactor}.log 2>&1 &
+nohup ./run_formal.sh ${dataset} ${ffactor} ${fvariable} > run_formal_${dataset}_${ffactor}_${fvariable}.log 2>&1 &
