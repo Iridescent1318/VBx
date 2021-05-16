ab38648a59e738a4acfda29941e64a9cee39aad9
Iridescent <lzty19981106@qq.com>
2021-05-16 10:55:56 +0800

    optimize the pr curve calculation using pandas

--- spkrd_test_audio/calc_roc.py
+++ spkrd_test_audio/calc_roc.py
@@ -17,34 +17,72 @@ def draw_eer_curve(plt, table, legend):
 
 
 def calc_prec_recall(table, n=100, fi=None):
-    max_ll, min_ll = table[:][3].max(), table[:][3].min()
+    max_ll, min_ll = table[:]['plda_val'].max(), table[:]['plda_val'].min()
     step = (max_ll - min_ll) / (n + 1)
-    reg_set = set(table[:][2])
+    audiofile_to_spkr_dict = {}
+    spkr_group = table.groupby('fname')
+    for sg in spkr_group:
+        audiofile_to_spkr_dict[sg[0]] = set(sg[1][:]['sys'])
     prec_recall = [[0., 0., 0.] for _ in range(n)]
     for i in range(1, n + 1):
         if fi:
             print(f'Processing {fi}: {i} / {n}')
         cur_thrs = min_ll + i * step
         tp = fp = tn = fn = 0
-        for index, ins in table.iterrows():
-            if ins[1] in reg_set:
-                if ins[1] == ins[2]:
-                    # correctly predicted
-                    if ins[3] >= cur_thrs:
-                        tp += 1
-                    # missed
-                    else:
-                        fn += 1
-                # prediction in regset but wrong
-                else:
-                    fn += 1
-            else:
-                # predict nothing if not in the given set
-                if ins[3] < cur_thrs:
-                    tn += 1
-                else:
-                    fp += 1
-        prec_recall[i - 1] = [tp / (tp + fp), tp / (tp + fn), cur_thrs]
+        for sg in spkr_group:
+            group = sg[1]
+            gtp = group.apply(
+                lambda x: 1 if x['ref'] in audiofile_to_spkr_dict[sg[0]] and \
+                    x['ref'] == x['sys'] and \
+                    x['plda_val'] >= cur_thrs else 0,
+                axis=1
+            )
+            gfn = group.apply(
+                lambda x: 1 if (x['ref'] in audiofile_to_spkr_dict[sg[0]] and \
+                        x['ref'] == x['sys'] and \
+                        x['plda_val'] < cur_thrs
+                    ) or (
+                        x['ref'] in audiofile_to_spkr_dict[sg[0]] and \
+                        x['ref'] != x['sys']
+                    ) else 0,
+                axis=1
+            )
+            gtn = group.apply(
+                lambda x: 1 if x['ref'] not in audiofile_to_spkr_dict[sg[0]] and \
+                    x['plda_val'] < cur_thrs else 0,
+                axis=1
+            )
+            gfp = group.apply(
+                lambda x: 1 if x['ref'] not in audiofile_to_spkr_dict[sg[0]] and \
+                    x['plda_val'] >= cur_thrs else 0,
+                axis=1
+            )
+            tp += gtp.sum()
+            fp += gfp.sum()
+            tn += gtn.sum()
+            fn += gfn.sum()
+        # for index, ins in table.iterrows():
+        #     if ins['ref'] in audiofile_to_spkr_dict[ins['fname']]:
+        #         if ins['ref'] == ins['sys']:
+        #             # correctly predicted
+        #             if ins['plda_val'] >= cur_thrs:
+        #                 tp += 1
+        #             # missed
+        #             else:
+        #                 fn += 1
+        #         # prediction in regset but wrong
+        #         else:
+        #             fn += 1
+        #     else:
+        #         # predict nothing if not in the given set
+        #         if ins['plda_val'] < cur_thrs:
+        #             tn += 1
+        #         else:
+        #             fp += 1
+        if tp + fp != 0 and tp + fn != 0:
+            prec_recall[i - 1] = [tp / (tp + fp), tp / (tp + fn), cur_thrs]
+        else:
+            prec_recall[i - 1] = [0, 0, cur_thrs]
     return prec_recall       
 
 
@@ -69,8 +107,9 @@ if __name__ == '__main__':
         table = pd.read_csv(
             os.path.join(args.thrs_dir, fi),
             sep=' ',
+            names=['fname', 'ref', 'sys', 'plda_val'],
             header=None
         )
-        prec_recall = calc_prec_recall(table, 100, fi)
+        prec_recall = calc_prec_recall(table, 200, fi)
         prec_recall = pd.DataFrame(prec_recall, columns=['precision', 'recall', 'thrs'])
         prec_recall.to_csv(f'pr/{fi}', index=0, sep=' ')
